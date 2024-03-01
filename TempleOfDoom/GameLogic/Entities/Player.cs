f38fcac63438a28d4c4bb6a63c856a62d99a54c0
Delluci <wmn.brunenberg@student.avans.nl>
2024-03-01 17:50:06 +0100

    build(Observer): add player observer

--- TempleOfDoom/GameLogic/Entities/Player.cs
+++ TempleOfDoom/GameLogic/Entities/Player.cs
@@ -1,18 +1,19 @@
-﻿using GameLogic.Observers;
-using System;
-using System.Collections.Generic;
+﻿using GameLogic.Models;
+using GameLogic.Observers;
 
 namespace GameLogic.Entities {
     public class Player : IObservable<Player> {
-        private List<IObserver<Player>> observers;
+        private readonly List<IObserver<Player>> observers;
 
         public int StartRoomId { get; set; }
         public int StartX { get; set; }
         public int StartY { get; set; }
         public int Lives { get; set; }
+        public List<GameObject> Inventory { get; set; }
 
         public Player() {
             observers = new List<IObserver<Player>>();
+            Inventory = new List<GameObject>();
         }
 
         public IDisposable Subscribe(IObserver<Player> observer) {
--- TempleOfDoom/GameLogic/Observers/Unsubscriber.cs
+++ TempleOfDoom/GameLogic/Observers/Unsubscriber.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace GameLogic.Observers {
     public class Unsubscriber<Player> : IDisposable {
         public List<IObserver<Player>> _observers;
